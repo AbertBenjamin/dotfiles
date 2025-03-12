a308b197560fd713b7f4004db09f9a26d477c849
AbertBenjamin <benjamin.ostvang.abert@decisive.no>
2025-03-12 22:29:27 +0100

    legger til git change preview/hunk

--- nvim/.config/nvim/lua/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/plugins/git-signs.lua
@@ -1,7 +1,16 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup({})
+    local gitsigns = require("gitsigns")
+    gitsigns.setup()
+
+    vim.keymap.set("n", "g;", "g;:Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "g,", "g,:Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
+
+    -- Undo the shown hunk (reset changes)
+    vim.keymap.set("n", "<leader>gu", function()
+      gitsigns.reset_hunk()
+    end, { noremap = true, silent = true, desc = "Undo Git Hunk" })
   end
 
 }
