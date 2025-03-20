e523bca2dec3efe89718d2a259048118518d8431
AbertBenjamin <benjamin.ostvang.abert@decisive.no>
2025-03-20 16:00:21 +0100

    legger til treesitter context for function header on scroll

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,25 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+    dependencies = { "nvim-treesitter/nvim-treesitter-context"},
     config = function()
+      require("treesitter-context").setup({
+        enable = true,
+        max_lines = 3,
+        trim_scope = "inner",
+        mode = "cursor"
+      })
 
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "javascript", "kotlin", "java", "javascript", "toml",
-        "json", "bash", "css", "html", "sql", "markdown", "typescript", "xml" },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = {
+          "lua", "javascript", "kotlin", "java", "javascript", "toml",
+          "json", "bash", "css", "html", "sql", "markdown", "typescript", "xml"
+        },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true }
+      })
     end
   }
 }
