d7c3b802176fdf36f96614da9b1c9a0edb6d40b9
AbertBenjamin <benjamin.ostvang.abert@decisive.no>
2025-03-24 16:40:49 +0100

    utkommenterer dobbel brackets/ticks

--- nvim/.config/nvim/lua/vimrc.lua
+++ nvim/.config/nvim/lua/vimrc.lua
@@ -23,14 +23,14 @@ vim.opt.fillchars = { eob = " " }
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
-local opts = { noremap = true, silent = true }
-vim.keymap.set('i', '{', '{ }<Esc>ha', opts)
-vim.keymap.set('i', '[', '[ ]<Esc>ha', opts)
-vim.keymap.set('i', '(', '( )<Esc>ha', opts)
-vim.keymap.set('i', '"', '""<Esc>ha', opts)
-vim.keymap.set('i', "'", "''<Esc>ha", opts)
-vim.keymap.set('i', "`", "``<Esc>ha", opts)
-vim.keymap.set('n', "n", 'nzz', opts)
+-- local opts = { noremap = true, silent = true }
+-- vim.keymap.set('i', '{', '{ }<Esc>ha', opts)
+-- vim.keymap.set('i', '[', '[ ]<Esc>ha', opts)
+-- vim.keymap.set('i', '(', '( )<Esc>ha', opts)
+-- vim.keymap.set('i', '"', '""<Esc>ha', opts)
+-- vim.keymap.set('i', "'", "''<Esc>ha", opts)
+-- vim.keymap.set('i', "`", "``<Esc>ha", opts)
+-- vim.keymap.set('n', "n", 'nzz', opts)
 
 vim.keymap.set('n', '<leader>a', ':BlameToggle<CR>', opts)
 
