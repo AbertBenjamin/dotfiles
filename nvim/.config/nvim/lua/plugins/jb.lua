52e4ee98e6f9128261b59cb5d03743502e6404af
AbertBenjamin <benjamin.ostvang.abert@decisive.no>
2025-04-28 10:06:59 +0200

    merger inn fra remote

--- nvim/.config/nvim/lua/plugins/jb.lua
+++ nvim/.config/nvim/lua/plugins/jb.lua
@@ -1,12 +1,37 @@
+-- return {
+--   "nickkadutskyi/jb.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     require("jb").setup({
+--       transparent = true
+--     })
+--     vim.cmd("colorscheme jb")
+--   end,
+-- }
 return {
-  "nickkadutskyi/jb.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("jb").setup({
-      transparent = true
-    })
-    vim.cmd("colorscheme jb")
-  end,
+	{
+		"xiantang/darcula-dark.nvim",
+		config = function()
+			-- setup must be called before loading
+			require("darcula").setup({
+				override = function(c)
+					return {
+						background = "#333333",
+						dark = "#000000"
+					}
+				end,
+				opt = {
+					integrations = {
+						telescope = false,
+						lualine = true,
+						lsp_semantics_token = true,
+						nvim_cmp = true,
+						dap_nvim = true,
+					},
+				},
+			})
+		end,
+	},
 }
