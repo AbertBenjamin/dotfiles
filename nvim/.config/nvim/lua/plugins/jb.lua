6f113d9eb6d1542ab735b6d04fb41598ca73b8cc
AbertBenjamin <benjaminabert@outlook.com>
2025-04-30 17:51:19 +0200

    colorscheme

--- nvim/.config/nvim/lua/plugins/jb.lua
+++ nvim/.config/nvim/lua/plugins/jb.lua
@@ -1,12 +1,25 @@
-return {
-  "nickkadutskyi/jb.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require("jb").setup({
-      transparent = true
-    })
-    vim.cmd("colorscheme jb")
-  end,
-}
+return 
+-- {
+--         "nickkadutskyi/jb.nvim",
+--         lazy = false,
+--         priority = 1000,
+--         opts = {},
+--         config = function()
+--             require("jb").setup({
+--                 transparent = true
+--             })
+--             vim.cmd("colorscheme jb")
+--         end,
+--     },
+    {
+        "xiantang/darcula-dark.nvim",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("darcula").setup({
+                transparent = true,
+            })
+            vim.cmd.colorscheme("darcula-dark")
+        end,
+    }
