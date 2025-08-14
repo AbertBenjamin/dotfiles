01459ebe1d9482509ae9c06f54a9681efcf5ec3d
AbertBenjamin <benjamin.ostvang.abert@decisive.no>
2025-08-14 16:52:23 +0200

    fikser rainbow delimiter

--- nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
+++ nvim/.config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -1,3 +1,30 @@
 return {
-  "hiphish/rainbow-delimiters.nvim"
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    config = function()
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = "rainbow-delimiters.strategy.global",
+          vim = "rainbow-delimiters.strategy.local"
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks"
+        },
+        highlight = {
+          "RainBowDelimiterYellow",
+          "RainBowDelimiterGreen",
+          "RainBowDelimiterBlue",
+          "RainBowDelimiterViolet",
+          "RainBowDelimiterYellow",
+          "RainBowDelimiterGreen",
+          "RainBowDelimiterBlue",
+          "RainBowDelimiterViolet",
+        }
+      }
+    end
+  }
 }
+
