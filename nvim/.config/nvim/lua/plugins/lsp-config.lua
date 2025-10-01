b2c974bdf67074c34f3edfc80f03392722f0874f
AbertBenjamin <benjamin.ostvang.abert@decisive.no>
2025-10-01 17:51:06 +0200

    update lspconfig

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -64,40 +64,7 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "saghen/blink.cmp" },
-    opts = {
-      servers = {
-        java_language_server = {},
-        bashls = {},
-        docker_compose_language_service = {},
-        dockerls = {},
-        eslint = {},
-        jsonls = {},
-        lemminx = {},
-        ts_ls = {},
-        yamlls = {},
-        gopls = {},
-        lua_ls = {}
-      },
-    },
-    config = function(_, opts)
-      local lspconfig = require("lspconfig")
-
-      for server, config in pairs(opts.servers) do
-        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
-      end
-
-      local opts = { silent = true }
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set({ "i" }, "<M-CR>", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-
-    end
+    dependencies = { "saghen/blink.cmp" }
   },
   {
     "nvimtools/none-ls.nvim",
