92a3124cd831609d2455e5724fa4dde0dabe3394
AbertBenjamin <benjamin.ostvang.abert@decisive.no>
2026-04-14 20:10:07 +0200

    update fff

--- nvim/.config/nvim/lua/plugins/fff.lua
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -12,20 +12,24 @@ return {
       enabled = true,     -- we expect your collaboration at least during the beta
       show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
     },
+    layout = {
+      prompt_position = 'top'
+    },
   },
   -- No need to lazy-load with lazy.nvim.
   -- This plugin initializes itself lazily.
   lazy = false,
   keys = {
     {
-      "ff", -- try it if you didn't it is a banger keybinding for a picker
+      "<leader>ff", -- try it if you didn't it is a banger keybinding for a picker
       function() require('fff').find_files() end,
       desc = 'FFFind files',
     },
     {
-      "fg",
+      "<leader>fg",
       function() require('fff').live_grep() end,
 
     }
-  }
+  },
+
 }
